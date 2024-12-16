5dbdf41a5f8d9855b79ba42c7f72ca106efdf21f
tmp <>
2024-12-16 06:09:37 -0500

    asdf

--- test.py
+++ test.py
@@ -1,9 +1,42 @@
 AzureDiagnostics
 | where ResourceType == "SEARCHSERVICES"
-| where OperationName == "SearchQuery"
+| where OperationName == "Query.Search"
+| where TimeGenerated >= ago(7d)
 | summarize 
     TotalQueries = count(),
-    SuccessfulQueries = countif(HttpStatusCode == 200)
-    by bin(TimeGenerated, 1h)
+    SuccessfulQueries = countif(resultSignature_d == 200),
+    FailedQueries = countif(resultSignature_d != 200)
+    by IndexName_s, bin(TimeGenerated, 1h)
+| extend 
+    SuccessRate = (todouble(SuccessfulQueries) / todouble(TotalQueries)) * 100,
+    FailureRate = (todouble(FailedQueries) / todouble(TotalQueries)) * 100
+| project TimeGenerated, IndexName_s, SuccessRate, FailureRate, TotalQueries
+
+
+AzureDiagnostics
+| where ResourceType == "SEARCHSERVICES"
+| where OperationName == "Query.Search"
+| where TimeGenerated >= ago(7d)
+| summarize CountByStatus = count() by IndexName_s, resultSignature_d
+| project IndexName_s, resultSignature_d, CountByStatus
+
+
+AzureDiagnostics
+| where ResourceType == "SEARCHSERVICES"
+| where OperationName == "Query.Search"
+| where resultSignature_d == 429
+| where TimeGenerated >= ago(7d)
+| summarize ThrottledQueries = count() by IndexName_s, bin(TimeGenerated, 1h)
+| project TimeGenerated, IndexName_s, ThrottledQueries
+
+
+AzureDiagnostics
+| where ResourceType == "SEARCHSERVICES"
+| where OperationName == "Query.Search"
+| where TimeGenerated >= ago(7d)
+| summarize 
+    TotalQueries = count(),
+    SuccessfulQueries = countif(resultSignature_d == 200)
+    by IndexName_s, bin(TimeGenerated, 1h)
 | extend ServiceAvailability = (todouble(SuccessfulQueries) / todouble(TotalQueries)) * 100
-| project TimeGenerated, ServiceAvailability
+| project TimeGenerated, IndexName_s, ServiceAvailability
